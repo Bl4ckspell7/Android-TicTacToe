0e05458c62887709a0814d37d3a7731364a3232e
Bl4ckspell7 <bl4ckspell@proton.me>
2023-07-05 16:10:51 +0200

    Initial commit

---
+++ app/src/main/java/bl4ckspell7/tictactoe/MainActivity.java
@@ -0,0 +1,14 @@
+package bl4ckspell7.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
